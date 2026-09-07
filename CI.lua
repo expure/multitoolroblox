d5863157c1d3e3ef92e091ccb975fd9b327722f7
expure <rwapppwr@gmail.com>
2026-09-07 13:39:31 +0500

    Update CI.lua

--- CI.lua
+++ CI.lua
@@ -7,7 +7,7 @@ local StarterGui = game:GetService("StarterGui")
 local UserInputService = game:GetService("UserInputService")
 if  _G.sourcecheckexvs ~= "source" then 
     warn("https://rscripts.net/script/multi-tool-hub-X0cu")
-    error("The script source is not supported! Please use EXVS Hub from link upper!")
+    error("The script source is not supported! Please use EXVS Hub from link upper!", 0)
 end
 local CONFIG = {
 	MELEE_TOOLTIP = "Melee Weapon",
