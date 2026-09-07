d37dcb0507a4233c41004d8165f9c3cd249c5e02
expure <rwapppwr@gmail.com>
2026-09-07 13:30:41 +0500

    Update SMS.lua

--- SMS.lua
+++ SMS.lua
@@ -11,7 +11,10 @@ local humanoid = character:WaitForChild("Humanoid")
 local hrp = character:WaitForChild("HumanoidRootPart")
 local head = character:WaitForChild("Head")
 local Mouse = player:GetMouse()
-
+if  _G.sourcecheckexvs ~= "source" then 
+    warn("https://rscripts.net/script/multi-tool-hub-X0cu")
+    error("The script source is not supported! Please use EXVS Hub from link upper!")
+end
 local BASE_WALK_SPEED = humanoid.WalkSpeed
 local BASE_JUMP = humanoid.JumpPower
 
